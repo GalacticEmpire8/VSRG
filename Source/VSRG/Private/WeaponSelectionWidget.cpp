c74cd897868dfd012201f24b9a0aa83364d306f7
Tyler Kemp <tylerctk@gmail.com>
2025-05-19 16:16:13 +1200

    changes

--- Source/VSRG/Private/WeaponSelectionWidget.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "WeaponSelectionWidget.h"
-
-void UWeaponSelectionWidget::InitWeaponOptions(const TArray<UAttackBase*>& Options)
-{
-    WeaponOptions = Options;
-}
